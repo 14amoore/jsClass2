80292e40227b9356363204275cbfa335d35c7c39
Adam Moore <adamoore@gmail.com>
2019-08-07 13:03:26 -0400

    added mouseover

--- js/index.js
+++ js/index.js
@@ -7,4 +7,12 @@ document.getElementById("demoButton").onclick = function getTime() {
   document.getElementById("demoPara").innerHTML = Date();
 };
 
-Object.addEventListener("mouseover", bigCat);
+function bigCat(x) {
+  x.style.height = "331px";
+  x.style.width = "500px";
+}
+
+function normalCat(x) {
+  x.style.width = "166px";
+  x.style.height = "250px";
+}
